7fcddde7f8e5ccef780888865c07b8ddc9df66a6
tgugushvili <t.gugushvili@hotmail.com>
2021-04-25 00:15:26 +0400

    ggplot2Shiny

---
+++ app.R
@@ -0,0 +1,101 @@
+
+#libraries
+
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(tidyverse)
+
+
+#import data
+
+tg_tourism <- read.csv("data/tourism_data.csv")
+
+#variable
+
+color <- c("type_en", "Tourist")
+
+
+# Define UI 
+ui <- fluidPage(
+
+# Application title
+titlePanel("ggplot2Shiny Workshop"),
+
+# Sidebar with control
+sidebarLayout(
+    sidebarPanel(
+            selectizeInput(inputId = "visits_type", 
+                           label = "Select Type of Visits:",
+                           choices = unique(tg_tourism$type_en),
+                           selected = c("International Traveller Trips", "Same Day Trips"),
+                           multiple = T),
+         
+            
+            sliderInput(inputId = "year_filter",
+                        label = "Filter Year",
+                        min = 1995,
+                        max = 2020,
+                        value = c(1995, 2019),
+                        sep = ""),
+            
+            
+        ),
+
+# Show a plot ggplot and  plotly
+mainPanel(
+ tabsetPanel(
+     tabPanel("ggplot2",
+              plotOutput("tourism_ggplot")
+            ),
+            
+    tabPanel("plotly", 
+             plotlyOutput("tourism_plotly")
+            )
+))))
+
+# Define server 
+server <- function(input, output) {
+
+tg_tourism_filter <- reactive({
+        minyear <- input$year_filter[1]
+        maxyear <- input$year_filter[2]
+        
+        tg_tourism %>%
+            dplyr::filter(year >= minyear, year <= maxyear) %>%
+            dplyr::filter(type_en %in% input$visits_type)
+                       })
+
+
+output$tourism_plotly  <- renderPlotly({
+    
+tourism_digram <- ggplot2::ggplot(tg_tourism_filter(), aes_string("year", "visits", group = "type_en", color = "Tourist"))+
+                  geom_line()+
+                  geom_point()+
+                  theme_minimal(base_family="Sylfaen")+
+                  scale_x_continuous(breaks=seq(1995, 2020, 1))+
+                  scale_y_continuous(breaks=seq(0, 10000000, 1000000), 
+                                     labels = scales::comma)
+    
+    plotly::ggplotly(tourism_digram)
+    
+}) 
+
+
+output$tourism_ggplot  <- renderPlot({
+    
+        ggplot2::ggplot(tg_tourism_filter(), aes_string("year", "visits", group = "type_en", color = "Tourist"))+
+        geom_line()+
+        geom_point()+
+        theme_minimal(base_family="Sylfaen")+
+        scale_x_continuous(breaks=seq(1995, 2020, 1))+
+        scale_y_continuous(breaks=seq(0, 10000000, 1000000), 
+                           labels = scales::comma)
+    
+}) 
+
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
